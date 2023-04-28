27e2ca80653e821179d121adaeb55173d811a3ae
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-28 23:42:50 +0330

    Create media-uploader-ajax.php

---
+++ media-uploader-ajax.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Media Uploader Ajax Type Sample Plugin
+Plugin URI: https://github.com/mahdigosyan/ajax-media-uploader
+Description: Media uploader ajax type sample plugin.
+Version: 1.0.0
+Author: Mahdi Gosyan
+License: GPLv2 or later
+*/
+
