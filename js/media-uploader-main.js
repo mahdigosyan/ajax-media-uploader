000c01b4308fe2777c7a419201a7805b0c440484
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-29 23:40:34 +0330

    Update media-uploader-main.js

--- js/media-uploader-main.js
+++ js/media-uploader-main.js
@@ -10,4 +10,18 @@
 			},
 			multiple: false
 		});
-        
+        custom_uploader.on("select", function () {
+			var images = custom_uploader.state().get('selection');
+
+			images.each(function(file) {
+				$("#image-url").val(file.toJSON().url);
+				$("#image-view").attr("src", file.toJSON().url);
+			});
+		});
+
+		$("#media-upload").on("click", function(e) {
+			e.preventDefault();
+			custom_uploader.open();
+		});
+	});
+})(jQuery);
